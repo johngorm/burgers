7d5bb7cd8aed81965f62fadb18c6395db2fabed0
John Gorman <johngorm@usc.edu>
2017-05-07 19:41:38 -0500

    Code to initialize Database

---
+++ db/seed.sql
@@ -0,0 +1,11 @@
+INSERT INTO burgers (burger_name)
+VALUES 'Bacon Chedder Burger';
+
+INSERT INTO burgers (burger_name)
+VALUES 'Black Bean Burger'
+
+INSERT INTO burgers (burger_name)
+VALUES 'Kobe Beef Burger with Salmon Roe'
+
+
+
