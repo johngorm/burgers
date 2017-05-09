5b8815c0fd75fc94982872b4e449a2c39f4b806c
John Gorman <johngorm@usc.edu>
2017-05-08 22:46:26 -0500

    Use Sequelize for burger model

---
+++ models/burger.js
@@ -0,0 +1,22 @@
+//Dependncies
+const Sequelize = require('sequelize');
+
+const sequelize_connect = require('./config/connection.js');
+
+let Burger = sequelize_connect.define('burger',{
+	burger_name:{
+		type: Sequelize.STRING
+	},
+	devoured:{
+		type: Sequelize.BOOLEAN
+	},
+	date:{
+		type: Sequelize.DATE
+	}
+}, {
+	timestamps: false
+});
+
+Burger.sync();
+
+module.exports = Burger;
