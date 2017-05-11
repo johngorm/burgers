ffbca96dd98bb3bc7e8a1b8bb2d70c7ca84ecd90
John Gorman <johngorm@usc.edu>
2017-05-11 17:57:28 -0500

    Remove comments

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -17,11 +17,11 @@ router.get('/', function(req, res){
 	//route to add new burger to webpage
 router.post('/', function(req, res){
 
-	Burger.create({ burger_name : req.body.burger_name})
-		.then(function(result){
-			console.log(result)
-			res.redirect('/');
-		});
+	Burger.create({ 
+		burger_name : req.body.burger_name
+	}).then(function(result){
+		res.redirect('/');
+	});
 });
 //route to change burger status "devoured" to true
 
@@ -38,32 +38,3 @@ router.put('/:id', function(req, res){
 });
 
 module.exports = router;
-
-
-
-// Burger.update(
-// 	{ devoured: false},
-// 	{ where: 
-// 		{
-// 			id: 3
-// 		}
-// 	}).spread(function(result, affectedRows){
-// 		console.log('affectedRows', affectedRows)
-// 		return Burger.findAll();
-// 	}).then(function(burgers){
-// 		console.log(burgers);
-// 	});
-
-// Burger.findAll({})
-// 	.then(function(result){
-// 		console.log(JSON.parse(JSON.stringify(result)));
-// 	})
-
-// Burger.findAll({
-// 	where:{
-// 		burger_name: 'Black Bean Burger'
-// 	}
-// }).then(function(result){
-// 	console.log(JSON.parse(JSON.stringify(result)));
-// });
-
