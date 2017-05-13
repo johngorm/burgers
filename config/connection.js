abecba207a6a45bb8a9381da616140c511e0c5ef
John Gorman <johngorm@usc.edu>
2017-05-13 09:45:43 -0500

    Heroku connect to JAWS_DB

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,12 @@
 // Dependencies
 var Sequelize = require("sequelize");
 const config = require('./config.js');
-
+let sequel_connect;
+if(process.env.JAWSDB_URL){
+	sequel_connect = new Sequelize(process.env.JAWSDB_URL);
+}
+else{
+	sequel_connect = new Sequelize(config);
+}
 // Creates mySQL connection using Sequelize and export
-module.exports = new Sequelize(config);
-
+module.exports = sequel_connect;
